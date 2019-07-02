bdf7e85040596adba821d0a2837ca684553db664
langheran <langheran@gmail.com>
2019-07-02 12:13:41 -0500

    changes

--- test_fr_comia.cpp
+++ test_fr_comia.cpp
@@ -9,3 +9,12 @@ static void help_fr_comia()
     std::cout << "\nPress any key to exit" << std::endl
               << std::endl;
 }
+
+const char* keys_fr_comia = 
+{
+    "{image | ../../resources/bald_guys.jpg}"
+    "{cascade | ../../resources/bald_guys.jpg}"
+    "{shape_predictor | ../../resources/bald_guys.jpg}"
+    "{res_net | ../../resources/bald_guys.jpg}"
+
+}
