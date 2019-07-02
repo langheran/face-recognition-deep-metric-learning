1e38f71e66df92967510fb32b2d1e2bb1202e2b1
langheran <langheran@gmail.com>
2019-07-02 12:56:37 -0500

    changes

--- test_fr_comia.cpp
+++ test_fr_comia.cpp
@@ -1,4 +1,16 @@
-#include <iostream>
+// #include <iostream>
+// #include <string>
+
+// #include <opencv2/opencv.hpp>
+// #include <opencv2/core/utility.hpp>
+
+#include <opencv2/opencv.hpp>
+#include <string>
+
+#include <dlib/dnn.h>
+#include <dlib/image_io.h>
+#include <dlib/misc_api.h>
+#include <dlib/opencv.h>
 
 static void help_fr_comia()
 {
@@ -10,17 +22,37 @@ static void help_fr_comia()
               << std::endl;
 }
 
-const char* keys_fr_comia = 
-{
-    "{image | ../../resources/bald_guys.jpg}"
-    "{cascade | ../../resources/haarcascade_frontalface_default.xml}"
-    "{shape_predictor | ../../shape_predictor_68_face_landmarks.dat}"
-    "{res_net | ../../resources/dlib_face_recognition_resnet_model_v1.dat}"
-}
+const char *keys_fr_comia =
+    {
+        "{image|../../resources/bald_guys.jpg}"
+        "{cascade|../../resources/haarcascade_frontalface_default.xml}"
+        "{shape_predictor|../../shape_predictor_68_face_landmarks.dat}"
+        "{res_net|../../resources/dlib_face_recognition_resnet_model_v1.dat}"};
 
-int main(int argc, const char * argv[])
+int main(int argc, const char *argv[])
 {
     help_fr_comia();
+    try
+    {
+        cv::CommandLineParser parser(argc, argv, keys_fr_comia);
+        std::string fimage = parser.get<std::string>("image");
+        std::cout << "Loading "
+                  << "'" << fimage << "'"
+                  << " image...";
+    }
+    catch (const std::exception &ex)
+    {
+        std::cerr << ex.what() << '\n';
+    }
+    catch (const std::string &ex)
+    {
+        std::cerr << ex << '\n';
+    }
+    catch (...)
+    {
+        std::cerr << "Error" << '\n';
+    }
 
-    
+    fflush(stdout);
+    std::cin.get();
 }
